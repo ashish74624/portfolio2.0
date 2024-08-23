dc3d30b34631c4b8839867c6e66b97f5eefdf73d
Ashish <ashish74624@gmail.com>
2024-08-23 22:51:13 +0530

    Contact me section complete

--- app/sections/Contact.tsx
+++ app/sections/Contact.tsx
@@ -1,5 +1,8 @@
 "use client"
 import Heading from '@/Components/Heading';
+import Github from '@/Icons/Github';
+import Linkedin from '@/Icons/Linkedin';
+import Mail from '@/Icons/Mail';
 import React from 'react'
 import { useState, ChangeEvent, FormEvent } from 'react';
 
@@ -46,10 +49,10 @@ export default function Contact() {
         }
     };
     return (
-        <section className='w-full z-50'>
+        <section className='w-full z-50 pb-10'>
             <Heading heading='Contact Me' />
-            <div className='flex '>
-                <form onSubmit={handleSubmit} className='p-4 bg-gray-500 w-max mx-auto rounded-md '>
+            <div className='flex w-max mx-auto gap-16 mt-8'>
+                <form onSubmit={handleSubmit} className='p-4 bg-black/60 border border-white/[0.2] w-max mx-auto rounded-2xl'>
                     <div className='flex flex-col'>
                         <InputFeild id='name' placeholder='Name' handleChange={handleChange} />
                     </div>
@@ -62,17 +65,22 @@ export default function Contact() {
                             name="message"
                             onChange={handleChange}
                             required
-                            className='text-black bg-gray-700 w-96 rounded-md min-h-32 px-1 py-1.5'
+                            className='text-white bg-gray-900 w-96 rounded-md min-h-32 px-2 py-1.5'
                             placeholder='Message'
                         ></textarea>
                     </div>
-                    <button className='w-full bg-white py-2 rounded-md mt-2' type="submit">Send</button>
+                    <button className='w-full bg-white py-2 rounded-2xl mt-2' type="submit">Send</button>
                     <p>{status}</p>
                 </form>
-                <div>
-                    <p>
-                        I&apos;d love to hear from you! Whether you have a question, feedback, or just want to connect, feel free to reach out using the form below
+                <div className='text-gray-200 w-[500px] '>
+                    <p className='text-justify'>
+                        I&apos;d love to hear from you! Whether you have a question, feedback, or just want to connect, feel free to reach out.
                     </p>
+                    <div className='mt-2'>
+                        <p className='flex gap-2 h-max items-center'><Mail /> ashish74624@gmail.com</p>
+                        <a href='https://www.linkedin.com/in/ashish74624' target="_blank" className='flex gap-2 h-max items-center'><Linkedin />  www.linkedin.com/in/ashish74624</a>
+                        <a href='https://github.com/ashish74624' target='_blank' className='flex gap-2 h-max items-center'><Github /> github.com/ashish74624</a>
+                    </div>
                 </div>
             </div>
         </section>
@@ -93,7 +101,7 @@ function InputFeild({ id, handleChange, placeholder }: InputFeildProps) {
             name={id}
             onChange={handleChange}
             required
-            className='text-black w-96 px-1 py-1.5 mb-2 rounded-md bg-gray-700'
+            className='text-white w-96 px-2 mb-2 rounded-2xl h-10 bg-gray-900'
             placeholder={placeholder}
         />
     )
